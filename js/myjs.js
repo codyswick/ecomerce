68a7f8b1adb5f290c1143308279aa3c63df3ad43
Cody Laptop <codyswick1988@gmail.com>
2019-03-06 20:36:14 -0600

    s

---
+++ js/myjs.js
@@ -0,0 +1,15 @@
+function addtocart(){
+
+  var y =parseInt(document.getElementById("amount").value);
+  var x = parseInt(document.getElementById("quant").innerHTML);
+
+  if(y>0){
+  document.getElementById("quant").innerHTML = x+y;}
+  else{
+    alert('You Buy Now');
+  }
+
+
+
+
+}
