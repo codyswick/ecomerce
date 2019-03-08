03896e75c958b132a0f3ddddf3a07b8b06b04f4d
codyswick <47154623+codyswick@users.noreply.github.com>
2019-03-08 07:03:01 -0600

    modal

--- js/myjs.js
+++ js/myjs.js
@@ -1,24 +1,13 @@
+
 function addtocart(){
 
   var y =parseInt(document.getElementById("amount").value);
   var x = parseInt(document.getElementById("quant").innerHTML);
 
   if(y>0){
-  document.getElementById("quant").innerHTML = x+y;}
+  document.getElementById("quant").innerHTML = x+y;
+  }
   else{
     alert('You Buy Now');
   }
-
-
-
-
-}
-
-
-function active(){
-  alert('as');
-  var element = document.getElementById("home");
-  element.classList.remove("active");
-}
-
 }
